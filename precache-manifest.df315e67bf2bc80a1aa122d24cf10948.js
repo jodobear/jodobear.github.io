3fc7dd9a68266cb24c75666202325ec94bfd8f05
jodobear <jodobear@protonmail.com>
2020-02-12 17:15:11 +0100

    Updates

---
+++ precache-manifest.df315e67bf2bc80a1aa122d24cf10948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa2acb3fbc6c20f40f1237c764d3edf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "710f9759fddff5b430b3",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e2f96ea1871d6cc2c723",
+    "url": "/static/js/2.01417c68.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.01417c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710f9759fddff5b430b3",
+    "url": "/static/js/main.fa0817df.chunk.js"
+  },
+  {
+    "revision": "2d9aaa07735ac5f72785",
+    "url": "/static/js/runtime-main.5cf6e0c1.js"
+  }
+]);
